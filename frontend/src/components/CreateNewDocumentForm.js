be07a07bcdaa7d5bcc3bf75a9cabc9935487d81a
Martin Mwangi <mwangi.martin24@gmail.com>
2022-07-31 23:04:21 +0300

    Add create new document form  component

--- frontend/src/components/CreateNewDocumentForm.js
+++ frontend/src/components/CreateNewDocumentForm.js
@@ -1,68 +1,97 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function CreateNewDocumentForm(){
-    const [document, setDocument] = useState({
-      owner:"",
-      title:"",
-      description:"",
-      fileUrl:""
+function CreateNewDocumentForm() {
+  const [owner, setOwner] = useState("")
+  const [title, setTitle] = useState("")
+  const [description, setDescription] = useState("")
+  const [fileUrl, setFileUrl] = useState(null)
+
+  //let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('owner', owner);
+    formData.append('title', title);
+    formData.append('description', description);
+    if (fileUrl !== null) {
+      formData.append('fileUrl', fileUrl);
+
+    }
+//    console.log(formData)
+
+    await axios({
+      method: "POST",
+      url: "http://localhost:4000/api/documents",
+      data: formData,
+      headers: { 'content-type': 'multipart/form-data' }
+    }).then((response) => {
+      console.log(response.data);
+      //navigate.push('/');
     })
-    
-    function handleSubmit(e){
-        e.preventDefault();
-        fetch("http://localhost:3000/documents",{
-          method: "POST",
-          headers:{
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            owner: e.target.owner.value,
-            title: e.target.title.value,
-            description: e.target.description.value,
-            fileUrl: e.target.fileUrl.value,
-          })
-        })
-        .then(response=>response.json())
-        .then(data=>console.log(data))
-        const test = {
-            owner: e.target.owner.value,
-            title: e.target.title.value,
-            description: e.target.description.value,
-            fileUrl: e.target.fileUrl.value,
-        }
-        console.log(test);
-    }
+    //   method: "POST",
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': undefined
 
-    function handleChange(e){
-        console.log(e.target.files[0]);
-    }
+    //   },
+    //   body: formData
+    // });
 
-    return(
-       <div className="col-12 d-flex justify-content-center text-center">
+    //     fetch("http://localhost:4000/documents",{
+    //       method: "POST",
+    //       headers:{
+    //         "Content-Type": "application/json"
+    //       },
+    //       body: JSON.stringify({
+    //         owner: e.target.owner.value,
+    //         title: e.target.title.value,
+    //         description: e.target.description.value,
+    //         fileUrl: e.target.fileUrl.value,
+    //       })
+    //     })
+    //     .then(response=>response.json())
+    //     .then(data=>console.log(data))
+
+
+  }
+
+  useEffect(()=>{
+    fetch("http://localhost:4000/api/documents")
+    .then(response=>response.json())
+    .then((results)=>{
+      results.documents.forEach((res)=>console.log(res))
+    })
+  })
+
+
+  return (
+    <div className="col-12 d-flex justify-content-center text-center">
        <form onSubmit={handleSubmit}>
-       <div className="mb-3">
+        <div className="mb-3">
           <h1>Create a new document</h1>
-          </div>
-  <div className="mb-3">
-    <label for="ownersname" className="form-label">Owner: </label>
-    <input type="text" className="form-control" onChange={handleChange} id="ownersname" name="owner" placeholder="Enter owner's name here..."/>
-  </div>
-  <div className="mb-3">
-    <label for="documentTitle" className="form-label">Title:</label>
-    <input type="text" className="form-control" onChange={handleChange} id="documentTitle" name="title" placeholder="Enter document title here..."/>
-  </div>
-  <div className="mb-3">
-    <label for="documentDescription" className="form-label">Description:</label>
-    <textarea className="form-control" onChange={handleChange} id="documentDescription" name="description" placeholder="Enter document description..."></textarea>
-  </div>
-  <div className="mb-3 form-check">
-  <label className="form-label" for="fileUpload">Upload document</label>
-<input type="file" className="form-control" onChange={handleChange} id="fileUpload" name="fileUrl" />
-  </div>
-  <button type="submit" className="btn btn-primary">Create document</button>
-</form>
-       </div>
-    )
+        </div>
+        <div className="mb-3">
+          <label htmlFor="ownersname" className="form-label">Owner: </label>
+          <input type="text" className="form-control" onChange={(e) => setOwner(e.target.value)} id="ownersname" name="owner" placeholder="Enter owner's name here..." />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="documentTitle" className="form-label">Title:</label>
+          <input type="text" className="form-control" onChange={(e) => setTitle(e.target.value)} id="documentTitle" name="title" placeholder="Enter document title here..." />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="documentDescription" className="form-label">Description:</label>
+          <textarea className="form-control" onChange={(e) => setDescription(e.target.value)} id="documentDescription" name="description" placeholder="Enter document description..."></textarea>
+        </div>
+        <div className="mb-3 form-check">
+          <label className="form-label" htmlFor="fileUpload">Upload document</label>
+          <input type="file" className="form-control" onChange={(e) => setFileUrl(e.target.files[0])} id="fileUpload" name="fileUrl" />
+        </div>
+        <button type="submit" className="btn btn-primary">Create document</button>
+      </form>
+    </div>
+  )
 }
 
 export default CreateNewDocumentForm;

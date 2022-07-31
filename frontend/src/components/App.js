5fb359d7de30d00b7d2f797e081feef29cd9eb3e
Martin Mwangi <mwangi.martin24@gmail.com>
2022-07-31 23:02:23 +0300

    Add App component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,14 +1,24 @@
+import React from 'react';
 import './../App.css';
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
 import NavBar from './NavBar';
 import CreateNewDocumentForm from './CreateNewDocumentForm';
-import Home from './Home'
+import Home from './Home';
+import DocumentsList from './DocumentsList';
+import Search from './Search';
+
 function App() {
   return (
-    <div className="App">
-        <CreateNewDocumentForm/>
-  </div>
 
+    <div className="App">
+      <NavBar/>
+       <Routes>
+       <Route exact path="/" element={<Home/>}></Route>
+      <Route exact path="/all-documents" element={<DocumentsList/>}></Route>
+      <Route exact path="/create-new-document" element={<CreateNewDocumentForm/>}></Route>
+      <Route exact path="/search" element={<Search/>}></Route> 
+      </Routes>
+  </div>
   );
 }
 

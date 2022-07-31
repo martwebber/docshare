bcd2e83441caa4bd24930080156c93b1d5e96842
Martin Mwangi <mwangi.martin24@gmail.com>
2022-07-31 23:12:38 +0300

    Add navbar component

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Link, Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { Container,Nav, Navbar } from 'react-bootstrap';
 
 function NavBar(){

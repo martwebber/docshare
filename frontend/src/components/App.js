0391cc81219cecceeb8a1cfffe5f43f52254a910
Martin Mwangi <mwangi.martin24@gmail.com>
2022-07-31 23:12:15 +0300

    Add App component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './../App.css';
-import { BrowserRouter, Route, Routes, Router } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import NavBar from './NavBar';
 import CreateNewDocumentForm from './CreateNewDocumentForm';
 import Home from './Home';

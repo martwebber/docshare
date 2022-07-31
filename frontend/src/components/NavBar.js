1d2609ed120731b20d36ec2f17ba9fff485389a1
Martin Mwangi <mwangi.martin24@gmail.com>
2022-07-31 23:05:37 +0300

    Add navbar component

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,28 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { BrowserRouter, Link, Router } from "react-router-dom";
+import { Container,Nav, Navbar } from 'react-bootstrap';
 
-function NavBar() {
-  return( 
-   <div className="navbar">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/add-document">Add New Document</NavLink>
-    </div>
-);
-};
+function NavBar(){
+    return (
+        
+            <div>        
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/">Home</Nav.Link>
+                <Nav.Link as={Link} to="/all-documents">All Documents</Nav.Link>
+                <Nav.Link as={Link} to="/create-new-document">Create New Document</Nav.Link>
+                <Nav.Link as={Link} to="/search">Search</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        </div>
+
+      );
+}
 
 export default NavBar;

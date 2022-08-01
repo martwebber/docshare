a957e18eb66709a14fea4c88174f8ba80c2c8b97
Martin Mwangi <mwangi.martin24@gmail.com>
2022-08-02 01:51:08 +0300

    Add ERemove commented code

--- frontend/src/components/CreateNewDocumentForm.js
+++ frontend/src/components/CreateNewDocumentForm.js
@@ -1,42 +1,42 @@
 import React, { useState } from 'react';
-//import axios from 'axios';
-
+import { useNavigate } from 'react-router-dom';
 function CreateNewDocumentForm() {
-  const [owner, setOwner] = useState("")
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const [fileUrl, setFileUrl] = useState(null)
+    const [formData, setFormData] = useState({
+      owner: "",
+      title: "",
+      description: "",
+      fileUrl: ""
+    });
+    let navigate = useNavigate();
 
-  //let navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    if ((fileUrl && title && description &&  owner) !== null) {
-      formData.append('owner', owner);
-      formData.append('title', title);
-      formData.append('description', description);
-      formData.append('fileUrl', fileUrl);
+    function handleSubmit(e){
+      e.preventDefault()
+      fetch('https://outgoing-sideways-spoonbill.glitch.me/documents', {
+        method: "POST",
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          owner: formData.owner,
+          title: formData.title,
+          description: formData.description,
+          fileUrl: formData.fileUrl
+        })
+      })
+      .then(response=>response.json())
+      .then(data=>{
+        console.log(data)
+      navigate("/all-documents")
+    })
     }
 
-    // await axios({
-    //   method: "POST",
-    //   url: "http://localhost:4000/api/documents",
-    //   data: formData,
-    //   headers: { 'content-type': 'multipart/form-data' }
-    // }).then((response) => {
-    //   console.log(response  );
-    //   //navigate.push('/');
-    // })
-
-    fetch("http://localhost:4000/api/documents",{
-      method: "POST",
-      body: formData
-    })
-    .then(response=>response.json())
-    .then(results=>console.log(results))
-  }
-
+    function handleChange(event) {
+      setFormData({
+        ...formData,
+        [event.target.name]: event.target.value,
+        
+      });
+    }
 
   return (
     <div className="col-12 d-flex justify-content-center text-center">
@@ -46,19 +46,19 @@ function CreateNewDocumentForm() {
         </div>
         <div className="mb-3">
           <label htmlFor="ownersname" className="form-label">Owner: </label>
-          <input type="text" className="form-control" onChange={(e) => setOwner(e.target.value)} id="ownersname" name="owner" placeholder="Enter owner's name here..." />
+          <input type="text" className="form-control" onChange={handleChange} id="ownersname" name="owner" placeholder="Enter owner's name here..." />
         </div>
         <div className="mb-3">
           <label htmlFor="documentTitle" className="form-label">Title:</label>
-          <input type="text" className="form-control" onChange={(e) => setTitle(e.target.value)} id="documentTitle" name="title" placeholder="Enter document title here..." />
+          <input type="text" className="form-control" onChange={handleChange} id="documentTitle" name="title" placeholder="Enter document title here..." />
         </div>
         <div className="mb-3">
           <label htmlFor="documentDescription" className="form-label">Description:</label>
-          <textarea className="form-control" onChange={(e) => setDescription(e.target.value)} id="documentDescription" name="description" placeholder="Enter document description..."></textarea>
+          <textarea className="form-control" onChange={handleChange} id="documentDescription" name="description" placeholder="Enter document description..."></textarea>
         </div>
         <div className="mb-3 form-check">
-          <label className="form-label" htmlFor="fileUpload">Upload document</label>
-          <input type="file" className="form-control" onChange={(e) => setFileUrl(e.target.files[0])} id="fileUpload" name="fileUrl" />
+          <label className="form-label" htmlFor="fileUpload">Upload document link</label>
+          <input type="text" className="form-control" onChange={handleChange} id="fileUpload" name="fileUrl" />
         </div>
         <button type="submit" className="btn btn-primary">Create document</button>
       </form>

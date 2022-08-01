0f9c042cebb5fcc386eb361e5e0da7e969a79c30
Martin Mwangi <mwangi.martin24@gmail.com>
2022-08-01 11:07:12 +0300

    Edit routes

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -6,7 +6,7 @@ import CreateNewDocumentForm from './CreateNewDocumentForm';
 import Home from './Home';
 import DocumentsList from './DocumentsList';
 import Search from './Search';
-
+import DocumentItem from './DocumentItem';
 function App() {
   return (
 
@@ -17,6 +17,7 @@ function App() {
       <Route exact path="/all-documents" element={<DocumentsList/>}></Route>
       <Route exact path="/create-new-document" element={<CreateNewDocumentForm/>}></Route>
       <Route exact path="/search" element={<Search/>}></Route> 
+      <Route exact path="/document/:_id" element={<DocumentItem/>}></Route> 
       </Routes>
   </div>
   );

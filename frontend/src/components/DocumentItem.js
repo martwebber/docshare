05d46e96a15aea8353946fc05d0a3e85277df8af
Martin Mwangi <mwangi.martin24@gmail.com>
2022-08-02 05:05:35 +0300

    Remove irrelevant code

--- frontend/src/components/DocumentItem.js
+++ frontend/src/components/DocumentItem.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import DocumentPage from './DocumentPage';
 
-function DocumentItem({document, handleDeleteClickEvent}){
-  //const [documents, setDocuments] = useState([]);
-  const {id, owner, title, description} = document;
+function DocumentItem({document}){
+  const {id, owner, title} = document;
 
-  <DocumentPage
-  onDeleteClick={handleDeleteClickEvent}
-  />
     return(
         <tr>
         <td>{owner}</td>
         <td>{title}</td>
-        <td>{description}</td>
         <td><Link to={`/document/${id}`}><button className="btn btn-primary">View more details</button></Link></td>
       </tr>
     )
